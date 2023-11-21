701ca0be9a5745a1430f07f56cb4b3b4b688153d
gems-yc4923 <yc4923@ic.ac.uk>
2023-11-21 17:59:59 +0000

    Updated read me file

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='yctmodel',
-    version='1.0.0',
+    version='1.1.0',
     packages=['yctmodel'],
-    install_requires=['sklearn','pandas','matplotlib',
+    install_requires=['scikit-learn','pandas','matplotlib',
                         'numpy','scipy','seaborn','xgboost',],
     url='https://github.com/gems-yc4923/thames.git',
     license='MIT',
